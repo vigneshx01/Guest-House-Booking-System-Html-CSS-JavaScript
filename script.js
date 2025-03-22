4d66bb9985d644fe19bf4f8b792ce9192b9ae91f
Vignesh061 <vigneshvicky66005@gmail.com>
2025-03-22 22:32:37 +0530

    Created Script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+// For Navbar
+
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+
+//   Scroll Animation 
+
+
+const scrollElements = document.querySelectorAll('.scroll-animate');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        }
+    });
+}, { threshold: 0.1 });
+
+scrollElements.forEach(el => observer.observe(el));
+
+
+
+
